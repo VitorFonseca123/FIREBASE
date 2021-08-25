867f39eb069507088bbd4c1b6ab31b99b6f30a7d
vitorfonseca123 <vitorfon20@gmail.com>
2021-08-25 15:05:53 -0300

    Upload teste firebase

---
+++ www/js/editar.js
@@ -0,0 +1,82 @@
+var app = {
+
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {
+        document.getElementById("btnBuscar").addEventListener("click",app.buscar);
+        document.getElementById("btnEditar").addEventListener("click",app.editar);
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        db = window.sqlitePlugin.openDatabase({
+            name: 'aplicativo.db',
+            location: 'default',            
+            androidDatabaseProvider: 'system'
+        });
+
+        db.transaction(function(tx) {
+            tx.executeSql('CREATE TABLE IF NOT EXISTS clientes (nome, telefone, origem, data_contato, observacao)');
+        }, function(error) {
+            console.log('Transaction ERROR: ' + error.message);
+        }, function() {
+            //alert('Banco e Tabela clientes criados com sucesso!!!');
+        });
+    },
+    
+    buscar: function(){
+        var url_string = window.location.href;
+        var url = new URL(url_string);
+        var getTelefone = url.searchParams.get("telefone");
+        //alert(getTelefone);
+        db.executeSql(
+            'SELECT nome AS uNome, telefone AS uTelefone, origem AS uOrigem, data_contato AS uDataContato, observacao ' + 
+            'AS uObservacao FROM clientes WHERE telefone = ?', [getTelefone], function(rs) {
+                //alert(JSON.stringify(rs));
+                //alert(rs.rows.length);
+                let i = 0;
+                for(i = 0; i < rs.rows.length; i++){
+                    //alert("item "+i);
+                    let recordItem = rs.rows.item(i);
+                    //alert(JSON.stringify(recordItem));
+                    document.getElementById("cadNome").value = rs.rows.item(i).uNome;
+                    document.getElementById("cadtelefone").value = rs.rows.item(i).uTelefone;
+                    document.getElementById("txtorigem").value = rs.rows.item(i).uOrigem;
+                    document.getElementById("cadData").value = rs.rows.item(i).uDataContato;
+                    document.getElementById("observacoes").value = rs.rows.item(i).uObservacao;
+                    
+                }
+            //alert('Record count (expected to be 2): ' + rs.rows.item(0).uLoginName);
+        }, function(error) {
+            //alert('Erro no SELECT: ' + error.message);
+        }); 
+    },
+
+    editar: function(){
+        var url_string = window.location.href;
+        var url = new URL(url_string);
+        var getTelefone = url.searchParams.get("telefone");
+        //alert(getTelefone);
+
+        let nome = document.getElementById("cadNome").value;
+        let telefone = document.getElementById("cadtelefone").value;
+        let origem = document.getElementById("txtorigem").value;
+        let data_contato = document.getElementById("cadData").value;
+        let observacao = document.getElementById("observacoes").value;
+
+        db.transaction(function(tx) {
+            tx.executeSql('UPDATE clientes SET nome=?, telefone=?, origem=?, data_contato=?, observacao=? WHERE telefone=?', [nome, telefone, origem, data_contato, observacao, getTelefone]);
+        }, function(error) {
+            //alert('Erro durante a transacao com o banco: ' + error.message);
+        }, function() {
+            //alert('Atualização realizada com sucesso!!!');
+        });
+    }
+
+};
+
+app.initialize();
